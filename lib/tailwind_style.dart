2ab05168bc8ae0288ec700f9c2ddba4cfca70bf6
bagasdany <bagasdany74@gmail.com>
2023-08-18 09:56:54 +0700

    Template package

---
+++ lib/tailwind_style.dart
@@ -0,0 +1,10 @@
+// library tailwind_style;
+
+// /// A Calculator.
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
+library tailwind_style;
+
+export 'src/tailwind_container.dart';
