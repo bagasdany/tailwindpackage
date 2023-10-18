86d3d76c81157411caace972e906415bda5648a0
bagasdany <bagasdany74@gmail.com>
2023-10-18 14:26:40 +0700

    margin fixs #1

--- lib/src/tailwind_container.dart
+++ lib/src/tailwind_container.dart
@@ -58,54 +58,114 @@ class _ContainerTailwindState extends State<ContainerTailwind> {
         }
       }
     }
-    List<String> classNames = (widget.extClass ?? "").split(" ");
+    List<String> classNames = (widget.extClass ?? "").split(" ").toList();
+      
+      //PADDING
 
       if (classNames.any((cls) => cls.startsWith("p-"))) {
         padding = EdgeInsets.all( getPaddingDouble("p-${int.parse(classNames.firstWhere((cls) => cls.startsWith("p-")).substring(2))}") ?? 0.0);  
-        } else if (classNames.any((cls) => cls.startsWith("px-")) && classNames.any((cls) => cls.startsWith("py-"))) {
-        double horizontalPaddingValue = getPaddingDouble("px-${(int.parse(classNames.firstWhere((cls) => cls.startsWith("px-")).substring(3)))}") ?? 0.0;
-        double verticalPaddingValue = getPaddingDouble("py-${(int.parse(classNames.firstWhere((cls) => cls.startsWith("py-")).substring(3)))}") ?? 0.0;
+      }
+      
+
+
+      else if(classNames.any((cls) => cls.startsWith("p") && cls.endsWith("auto") && !classNames.any((cls) => cls.startsWith("md:")) ) ) {
+        // margin = EdgeInsets.zero;
+        dynamic horizontal,vertical,left,right,top,bottom;
+        classNames == "p-auto" ? margin = EdgeInsets.zero:null;
+        classNames == "px-auto" ? horizontal= getPaddingDouble("px-0") :null;
+        classNames == "py-auto" ? vertical = getPaddingDouble("py-0"):null;
+        classNames == "pl-auto" ? left = getPaddingDouble("pl-0"):null;
+        classNames == "pr-auto" ? right = getPaddingDouble("pr-0"):null;
+        classNames == "pt-auto" ? top = getPaddingDouble("pt-0"):null;
+        classNames == "pb-auto" ? bottom = getPaddingDouble("pb-0"):null;
+        margin = EdgeInsets.fromLTRB(left ?? horizontal ?? 0, top ?? vertical ?? 0, right ?? horizontal ?? 0, bottom ?? vertical ?? 0); 
+        print("ini padding auto ${margin} classnamenya adalah ${classNames} & widget.extClass");
+        
+      }
+      else if (classNames.any((cls) => cls.startsWith("px-")) || classNames.any((cls) => cls.startsWith("py-"))) {
+      
+        double horizontalPaddingValue =classNames.toString().contains("px-") ? getPaddingDouble("px-${(int.parse((classNames.firstWhere((cls) => (cls.startsWith("px-"))).substring(3))))}") ?? 0.0 : 0.0;
+        double verticalPaddingValue =classNames.toString().contains("py-") ? getPaddingDouble("py-${(int.parse(classNames.firstWhere((cls) => cls.startsWith("py-")).substring(3)))}") ?? 0.0 : 0.0;
         padding = EdgeInsets.symmetric(horizontal: horizontalPaddingValue.toDouble(), vertical: verticalPaddingValue.toDouble());
-      } else if(classNames.any((cls) => cls.endsWith("auto")) && classNames.any((cls) => cls.endsWith("auto")) && !classNames.toString().contains("md:")) {
-       padding=  EdgeInsets.zero;
-      } else if(classNames.any((cls) => cls.startsWith("pl-"))) {
+        print("ini padding px ${margin} classnamenya adalah ${classNames} & widget.extClass");
+        
+      } 
+      else if(classNames.any((cls) => cls.startsWith("pl-"))) {
         var leftPadding = classNames.any((cls) => cls.startsWith("pl-")) ? getPaddingDouble("pl-${(int.parse(classNames.firstWhere((cls) => cls.startsWith("pl-")).substring(3)))}") ?? 0.0 : 0.0;
         var topPadding = classNames.any((cls) => cls.startsWith("pt-")) ? getPaddingDouble("pt-${(int.parse(classNames.firstWhere((cls) => cls.startsWith("pt-")).substring(3)))}") ?? 0.0 : 0.0;
         var rightPadding = classNames.any((cls) => cls.startsWith("pr-")) ? getPaddingDouble("pr-${(int.parse(classNames.firstWhere((cls) => cls.startsWith("pr-")).substring(3)))}") ?? 0.0 : 0.0;
         var bottomPadding = classNames.any((cls) => cls.startsWith("pb-")) ? getPaddingDouble("pb-${(int.parse(classNames.firstWhere((cls) => cls.startsWith("pb-")).substring(3)))}") ?? 0.0 : 0.0;
         padding = EdgeInsets.fromLTRB(leftPadding, topPadding, rightPadding, bottomPadding);
-        } 
-        else{
-          padding = widget.padding ?? const EdgeInsets.all(0);
-          
-        }
+        print("ini padding pl ${margin} classnamenya adalah ${classNames} & widget.extClass");
+        
+      } 
+      else{
+        print("ini padding ${widget.extClass}");
+        padding = widget.padding ?? const EdgeInsets.all(0);
+      }
+    
+    // MARGIN
 
     if (classNames.any((cls) => cls.startsWith("m-"))) {
-        margin = EdgeInsets.all( getMarginDouble("m-${int.parse(classNames.firstWhere((cls) => cls.startsWith("m-")).substring(2))}") ?? 0.0);  
-      }else if(classNames.any((cls) => cls.endsWith("auto")) && classNames.any((cls) => cls.endsWith("auto") && !classNames.any((cls) => cls.startsWith("md:")))) {
-        margin = EdgeInsets.zero;
-      }else if (classNames.any((cls) => cls.startsWith("mx-")) && classNames.any((cls) => cls.startsWith("my-") && !classNames.toString().contains("auto"))) {
+      margin = EdgeInsets.all( getMarginDouble("m-${int.parse(classNames.firstWhere((cls) => cls.startsWith("m-")).substring(2))}") ?? 0.0);  
+    }
+    else if (classNames.any((cls) => cls.startsWith("mx-")) && classNames.any((cls) => cls.startsWith("my-") )) {
+      if(classNames.any((cls) => cls.startsWith("m") && cls.endsWith("auto"))){
+        dynamic horizontal,vertical,left,right,top,bottom,horizontalMarginValue,verticalMarginValue;
+        classNames.toString().contains("m-auto") ? margin = EdgeInsets.zero:null;
+        classNames.toString().contains("mx-auto") ? horizontal= getMarginDouble("mx-0") : horizontalMarginValue = getMarginDouble("mx-${(int.parse(classNames.firstWhere((cls) => cls.startsWith("mx-")).substring(3)))}") ?? 0.0 ;
+        classNames.toString().contains("my-auto") ? vertical = getMarginDouble("my-0"):  verticalMarginValue = getMarginDouble("my-${(int.parse(classNames.firstWhere((cls) => cls.startsWith("my-")).substring(3)))}") ?? 0.0;
+        classNames.toString().contains("ml-auto") ? left = getMarginDouble("ml-0"):null;
+        classNames.toString().contains("mr-auto") ? right = getMarginDouble("mr-0"):null;
+        classNames.toString().contains("mt-auto") ? top = getMarginDouble("mt-0"):null;
+        classNames.toString().contains("mb-auto") ? bottom = getMarginDouble("mb-0"):null;
+        
+        margin = EdgeInsets.fromLTRB(left ?? horizontal ??  horizontalMarginValue.toDouble() ??0, top ?? vertical ??verticalMarginValue.toDouble()?? 0, right ?? horizontal ??horizontalMarginValue.toDouble() ?? 0, bottom ?? vertical ??verticalMarginValue.toDouble()?? 0); 
+        
+      }else{
+        
         double horizontalMarginValue = getMarginDouble("mx-${(int.parse(classNames.firstWhere((cls) => cls.startsWith("mx-")).substring(3)))}") ?? 0.0;
         double verticalMarginValue = getMarginDouble("my-${(int.parse(classNames.firstWhere((cls) => cls.startsWith("my-")).substring(3)))}") ?? 0.0;
         margin = EdgeInsets.symmetric(horizontal: horizontalMarginValue.toDouble(), vertical: verticalMarginValue.toDouble());
-      } 
-      else if(classNames.any((cls) => cls.startsWith("ml-")) ) {
-        var leftMargin = classNames.any((cls) => cls.startsWith("ml-")) ? getMarginDouble("ml-${(int.parse(classNames.firstWhere((cls) => cls.startsWith("ml-")).substring(3)))}") ?? 0.0 : 0.0;
-        var topMargin = classNames.any((cls) => cls.startsWith("mt-")) ? getMarginDouble("mt-${(int.parse(classNames.firstWhere((cls) => cls.startsWith("mt-")).substring(3)))}") ?? 0.0 : 0.0;
-        var rightMargin = classNames.any((cls) => cls.startsWith("mr-")) ? getMarginDouble("mr-${(int.parse(classNames.firstWhere((cls) => cls.startsWith("mr-")).substring(3)))}") ?? 0.0 : 0.0;
-        var bottomMargin = classNames.any((cls) => cls.startsWith("mb-")) ? getMarginDouble("mb-${(int.parse(classNames.firstWhere((cls) => cls.startsWith("mb-")).substring(3)))}") ?? 0.0 : 0.0;
-        margin = EdgeInsets.fromLTRB(leftMargin, topMargin, rightMargin, bottomMargin);
-        
       }
-      else{
-        margin = widget.margin  ?? const EdgeInsets.all(0);
-      }
+      
+    } 
+    else if(classNames.any((cls) => cls.startsWith("m") && cls.endsWith("auto")) && !classNames.any((cls) => cls.startsWith("md:"))) {
+      dynamic horizontal,vertical,left,right,top,bottom,horizontalMarginValue,verticalMarginValue;;
+      classNames.toString().contains("m-auto") ? margin = EdgeInsets.zero:null;
+      classNames.toString().contains("mx-auto") ? horizontal= getMarginDouble("mx-0") : horizontalMarginValue = getMarginDouble("mx-${(int.parse(classNames.firstWhere((cls) => cls.startsWith("mx-")).substring(3)))}") ?? 0.0 ;
+      classNames.toString().contains("my-auto") ? vertical = getMarginDouble("my-0"):  verticalMarginValue = getMarginDouble("my-${(int.parse(classNames.firstWhere((cls) => cls.startsWith("my-")).substring(3)))}") ?? 0.0;
+      classNames.toString().contains("ml-auto") ? left = getMarginDouble("ml-0"):null;
+      classNames.toString().contains("mr-auto") ? right = getMarginDouble("mr-0"):null;
+      classNames.toString().contains("mt-auto") ? top = getMarginDouble("mt-0"):null;
+      classNames.toString().contains("mb-auto") ? bottom = getMarginDouble("mb-0"):null;
+      margin = EdgeInsets.fromLTRB(left ?? horizontal ?? 0, top ?? vertical ?? 0, right ?? horizontal ?? 0, bottom ?? vertical ?? 0); 
+      
+    }
+    else if (classNames.any((cls) => cls.startsWith("mx-")) || classNames.any((cls) => cls.startsWith("my-")) ){
+      double horizontalMarginValue = getMarginDouble("mx-${(int.parse(classNames.firstWhere((cls) => cls.startsWith("mx-")).substring(3)))}") ?? 0.0;
+      double verticalMarginValue = getMarginDouble("my-${(int.parse(classNames.firstWhere((cls) => cls.startsWith("my-")).substring(3)))}") ?? 0.0;
+      margin = EdgeInsets.symmetric(horizontal: horizontalMarginValue.toDouble(), vertical: verticalMarginValue.toDouble());
+      
+    }
+    else if(classNames.any((cls) => cls.startsWith("ml-")) ) {
+      var leftMargin = classNames.any((cls) => cls.startsWith("ml-")) ? getMarginDouble("ml-${(int.parse(classNames.firstWhere((cls) => cls.startsWith("ml-")).substring(3)))}") ?? 0.0 : 0.0;
+      var topMargin = classNames.any((cls) => cls.startsWith("mt-")) ? getMarginDouble("mt-${(int.parse(classNames.firstWhere((cls) => cls.startsWith("mt-")).substring(3)))}") ?? 0.0 : 0.0;
+      var rightMargin = classNames.any((cls) => cls.startsWith("mr-")) ? getMarginDouble("mr-${(int.parse(classNames.firstWhere((cls) => cls.startsWith("mr-")).substring(3)))}") ?? 0.0 : 0.0;
+      var bottomMargin = classNames.any((cls) => cls.startsWith("mb-")) ? getMarginDouble("mb-${(int.parse(classNames.firstWhere((cls) => cls.startsWith("mb-")).substring(3)))}") ?? 0.0 : 0.0;
+      margin = EdgeInsets.fromLTRB(leftMargin, topMargin, rightMargin, bottomMargin);
+      
+    }
+    else{
+      margin = widget.margin  ?? const EdgeInsets.all(0);
+    }
 
     if (classNames.any((cls) => cls.startsWith("rounded-"))) {
-        borderRadius =( getRadius((classNames.firstWhere((cls) => cls.startsWith("rounded-")))) ); } 
+      borderRadius =( getRadius((classNames.firstWhere((cls) => cls.startsWith("rounded-")))) ); 
+    } 
     if (classNames.any((cls) => cls.startsWith("border-"))) {
-        borderColor =( getBorderColor((classNames.firstWhere((cls) => cls.startsWith("border-")))) );  
-       } 
+      borderColor =( getBorderColor((classNames.firstWhere((cls) => cls.startsWith("border-")))) );  
+    } 
 
 
     return ClipRRect(
